9b4ed751c103d17d1a857daee465185eef354c33
noam <37066741+NoamDev@users.noreply.github.com>
2026-02-25 10:14:27 +0200

    comment out additions to main

--- src/main.rs
+++ src/main.rs
@@ -306,56 +306,56 @@ fn run_language_server() {
     language_server::language_server_main::main()
 }
 
-fn visit_dirs(dir: &Path, cb: &mut dyn FnMut(&DirEntry)) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dirs(&path, cb)?;
-            } else {
-                cb(&entry);
-            }
-        }
-    }
-    Ok(())
-}
+// fn visit_dirs(dir: &Path, cb: &mut dyn FnMut(&DirEntry)) -> io::Result<()> {
+//     if dir.is_dir() {
+//         for entry in fs::read_dir(dir)? {
+//             let entry = entry?;
+//             let path = entry.path();
+//             if path.is_dir() {
+//                 visit_dirs(&path, cb)?;
+//             } else {
+//                 cb(&entry);
+//             }
+//         }
+//     }
+//     Ok(())
+// }
 
 
-pub fn test_all_files() -> Result<(), String> {
-    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    d.push("tests");
-    let mut all_results = vec![];
-    visit_dirs(&d, &mut |entry| {
-        if !matches!(
-            entry.path().extension().map(|x| x.to_str()),
-            Some(Some("par"))
-        ) {
-            return ();
-        }
-        eprintln!("Testing {:?}", entry.path());
-        let results = crate::test_runner::run_test_file(&entry.path(), &None, 10_000);
-        all_results.extend(
-            results
-                .into_iter()
-                .filter(|x| !x.status.is_passed())
-                .map(|x| (x, entry.path())),
-        )
-    })
-    .unwrap();
-    if all_results.is_empty() {
-        Ok(())
-    } else {
-        use std::fmt::Write;
-        let mut output = String::new();
-        writeln!(&mut output, "Some tests failed:").unwrap();
-        for (result, file) in all_results {
-            writeln!(&mut output, "{}: {:?}", file.display(), result.status).unwrap()
-        }
-        eprintln!("{}", output);
-        Err(output)
-    }
-}
+// pub fn test_all_files() -> Result<(), String> {
+//     let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+//     d.push("tests");
+//     let mut all_results = vec![];
+//     visit_dirs(&d, &mut |entry| {
+//         if !matches!(
+//             entry.path().extension().map(|x| x.to_str()),
+//             Some(Some("par"))
+//         ) {
+//             return ();
+//         }
+//         eprintln!("Testing {:?}", entry.path());
+//         let results = crate::test_runner::run_test_file(&entry.path(), &None, 10_000);
+//         all_results.extend(
+//             results
+//                 .into_iter()
+//                 .filter(|x| !x.status.is_passed())
+//                 .map(|x| (x, entry.path())),
+//         )
+//     })
+//     .unwrap();
+//     if all_results.is_empty() {
+//         Ok(())
+//     } else {
+//         use std::fmt::Write;
+//         let mut output = String::new();
+//         writeln!(&mut output, "Some tests failed:").unwrap();
+//         for (result, file) in all_results {
+//             writeln!(&mut output, "{}: {:?}", file.display(), result.status).unwrap()
+//         }
+//         eprintln!("{}", output);
+//         Err(output)
+//     }
+// }
 
 fn run_tests(file: Option<PathBuf>, filter: Option<String>, max_interactions: u32) {
     // test_all_files().unwrap();
